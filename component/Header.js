6402f24c3a755515d25e8612fae492251a39ffe6
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2023-04-02 19:57:40 -0700

    head update

--- component/Header.js
+++ component/Header.js
@@ -7,7 +7,11 @@ function Header() {
       <Head>
         <title>Startup Ideas, Sent Weekly</title>
 
-        <meta name="description" content="Startup Ideas, Sent Weekly" key="desc" />
+        <meta
+          name="description"
+          content="Startup Ideas, Sent Weekly. Join 1,000+ entrepreneurs getting curated startup ideas in million dollar niches."
+          key="desc"
+        />
         <meta property="og:title" content="Startup Ideas, Sent Weekly" />
         <meta
           property="og:description"
@@ -18,14 +22,14 @@ function Header() {
           property="og:image"
           content="https://i.postimg.cc/pTcnnb6p/Exploding-Ideas.png"
         />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-   
 
       <div class="flex flex-col justify-center -mt-8 items-center md:items-baseline -mb-10 md:-mb-56">
         <img
           src="/explodingideaslogo.png"
           className="max-w-sm scale-50 -mt-20 -mb-10"
-          alt="explodingideas logo"
+          alt="Startup Ideas, Sent Weekly"
         />
       </div>
     </div>
