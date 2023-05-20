ef1d66330fe40dd6838ab95687c709ddc4e0ba85
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2023-05-20 07:23:35 -0700

    gtm off

--- pages/_document.js
+++ pages/_document.js
@@ -16,14 +16,14 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
-          <noscript>
+          {/* <noscript>
             <iframe
               src="https://www.googletagmanager.com/ns.html?id=GTM-M3LGPKD"
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
-            ></iframe>
-          </noscript>
+            ></iframe> */}
+          {/* </noscript> */}
           <Main />
           <NextScript />
         </body>
